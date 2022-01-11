5124a9207303fd0bd1e2b5ae053786a50677e56e
cawo88 <cawo88@users.noreply.github.com>
2022-01-11 19:29:07 +0000

    Tokens updated

--- build/ios-swift/StyleDictionarySize.swift
+++ build/ios-swift/StyleDictionarySize.swift
@@ -4,60 +4,20 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 11 Jan 2022 13:38:09 GMT
+// Generated on Tue, 11 Jan 2022 19:29:06 GMT
 
 
 import UIKit
 
 public enum StyleDictionarySize {
-    public static let text2xlBoldFontWeight = 700
-    public static let text2xlLightFontWeight = 300
-    public static let text2xlMediumFontWeight = 500
     public static let text2xlRegularFontWeight = 400
-    public static let text2xlThinFontWeight = 100
-    public static let text3xlBoldFontWeight = 700
-    public static let text3xlLightFontWeight = 300
-    public static let text3xlMediumFontWeight = 500
     public static let text3xlRegularFontWeight = 400
-    public static let text3xlThinFontWeight = 100
-    public static let text4xlBoldFontWeight = 700
-    public static let text4xlLightFontWeight = 300
-    public static let text4xlMediumFontWeight = 500
     public static let text4xlRegularFontWeight = 400
-    public static let text4xlThinFontWeight = 100
-    public static let text5xlBoldFontWeight = 700
-    public static let text5xlLightFontWeight = 300
-    public static let text5xlMediumFontWeight = 500
     public static let text5xlRegularFontWeight = 400
-    public static let text5xlThinFontWeight = 100
-    public static let text6xlBoldFontWeight = 700
-    public static let text6xlLightFontWeight = 300
-    public static let text6xlMediumFontWeight = 500
     public static let text6xlRegularFontWeight = 400
-    public static let text6xlThinFontWeight = 100
-    public static let textBaseBoldFontWeight = 700
-    public static let textBaseLightFontWeight = 300
-    public static let textBaseMediumFontWeight = 500
     public static let textBaseRegularFontWeight = 400
-    public static let textBaseThinFontWeight = 100
-    public static let textLgBoldFontWeight = 700
-    public static let textLgLightFontWeight = 300
-    public static let textLgMediumFontWeight = 500
     public static let textLgRegularFontWeight = 400
-    public static let textLgThinFontWeight = 100
-    public static let textSmBoldFontWeight = 700
-    public static let textSmLightFontWeight = 300
-    public static let textSmMediumFontWeight = 500
     public static let textSmRegularFontWeight = 400
-    public static let textSmThinFontWeight = 100
-    public static let textXlBoldFontWeight = 700
-    public static let textXlLightFontWeight = 300
-    public static let textXlMediumFontWeight = 500
     public static let textXlRegularFontWeight = 400
-    public static let textXlThinFontWeight = 100
-    public static let textXsBoldFontWeight = 700
-    public static let textXsLightFontWeight = 300
-    public static let textXsMediumFontWeight = 500
     public static let textXsRegularFontWeight = 400
-    public static let textXsThinFontWeight = 100
 }
--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -4,59 +4,19 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 11 Jan 2022 13:38:09 GMT
+// Generated on Tue, 11 Jan 2022 19:29:06 GMT
 
 
 #import "StyleDictionarySize.h"
 
 
-float const TypographyTextXsThinFontWeight = 100;
-float const TypographyTextXsLightFontWeight = 300;
-float const TypographyTextXsRegularFontWeight = 400;
-float const TypographyTextXsMediumFontWeight = 500;
-float const TypographyTextXsBoldFontWeight = 700;
-float const TypographyTextSmThinFontWeight = 100;
-float const TypographyTextSmLightFontWeight = 300;
-float const TypographyTextSmRegularFontWeight = 400;
-float const TypographyTextSmMediumFontWeight = 500;
-float const TypographyTextSmBoldFontWeight = 700;
-float const TypographyTextBaseThinFontWeight = 100;
-float const TypographyTextBaseLightFontWeight = 300;
-float const TypographyTextBaseRegularFontWeight = 400;
-float const TypographyTextBaseMediumFontWeight = 500;
-float const TypographyTextBaseBoldFontWeight = 700;
-float const TypographyTextLgThinFontWeight = 100;
-float const TypographyTextLgLightFontWeight = 300;
-float const TypographyTextLgRegularFontWeight = 400;
-float const TypographyTextLgMediumFontWeight = 500;
-float const TypographyTextLgBoldFontWeight = 700;
-float const TypographyTextXlThinFontWeight = 100;
-float const TypographyTextXlLightFontWeight = 300;
-float const TypographyTextXlRegularFontWeight = 400;
-float const TypographyTextXlMediumFontWeight = 500;
-float const TypographyTextXlBoldFontWeight = 700;
-float const TypographyText2xlThinFontWeight = 100;
-float const TypographyText2xlLightFontWeight = 300;
-float const TypographyText2xlRegularFontWeight = 400;
-float const TypographyText2xlMediumFontWeight = 500;
-float const TypographyText2xlBoldFontWeight = 700;
-float const TypographyText3xlThinFontWeight = 100;
-float const TypographyText3xlLightFontWeight = 300;
-float const TypographyText3xlRegularFontWeight = 400;
-float const TypographyText3xlMediumFontWeight = 500;
-float const TypographyText3xlBoldFontWeight = 700;
-float const TypographyText4xlThinFontWeight = 100;
-float const TypographyText4xlLightFontWeight = 300;
-float const TypographyText4xlRegularFontWeight = 400;
-float const TypographyText4xlMediumFontWeight = 500;
-float const TypographyText4xlBoldFontWeight = 700;
-float const TypographyText5xlThinFontWeight = 100;
-float const TypographyText5xlLightFontWeight = 300;
-float const TypographyText5xlRegularFontWeight = 400;
-float const TypographyText5xlMediumFontWeight = 500;
-float const TypographyText5xlBoldFontWeight = 700;
-float const TypographyText6xlThinFontWeight = 100;
-float const TypographyText6xlLightFontWeight = 300;
 float const TypographyText6xlRegularFontWeight = 400;
-float const TypographyText6xlMediumFontWeight = 500;
-float const TypographyText6xlBoldFontWeight = 700;
+float const TypographyText5xlRegularFontWeight = 400;
+float const TypographyText4xlRegularFontWeight = 400;
+float const TypographyText3xlRegularFontWeight = 400;
+float const TypographyText2xlRegularFontWeight = 400;
+float const TypographyTextXlRegularFontWeight = 400;
+float const TypographyTextLgRegularFontWeight = 400;
+float const TypographyTextBaseRegularFontWeight = 400;
+float const TypographyTextSmRegularFontWeight = 400;
+float const TypographyTextXsRegularFontWeight = 400;
