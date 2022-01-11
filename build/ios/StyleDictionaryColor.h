d0a4c4ca221500411730f47d367c67f8085ec1e3
Carol Leung <carol.carl.leung@gmail.com>
2022-01-11 12:31:25 +0100

    chore: rebuild build folder

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Tue, 11 Jan 2022 11:30:31 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorColorsMultipleFills0,
+ColorColorsMultipleFills1,
+ColorColorsSingleBlue,
+ColorColorsRefBlue,
+ColorColorsSpecialCharacters,
+ColorColorsSpecialCharactersNderung,
+ColorLightBackground,
+ColorDarkBackground
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,17 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 11 Jan 2022 11:30:31 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const TypographyBodyH3FontWeight;
+extern float const TypographyBodyH4StrikeThroughFontWeight;
+extern float const TypographyBodyItalicFontWeight;
+extern float const TypographyBodyExtraBoldCondensedItalicFontWeight;
+extern float const TypographyBodyMediumExtendedItalicFontWeight;
+extern float const TypographyBodySuperFontWeight;
